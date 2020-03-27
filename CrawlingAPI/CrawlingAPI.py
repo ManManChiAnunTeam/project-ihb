dd65adaf2cd98e5d281cf05f1ed46dbb2e6fa38f
powergee <49356944+powergee@users.noreply.github.com>
2020-03-28 00:03:42 +0900

    Implement all functions in README

--- CrawlingAPI/CrawlingAPI.py
+++ CrawlingAPI/CrawlingAPI.py
@@ -1,5 +1,6 @@
 import BaekjoonAPI as bapi
 import SolvedAPI as sapi
+import json
 
 
 def searchID(id, pList):
@@ -18,17 +19,9 @@ def searchID(id, pList):
     return None
 
 
-def toJSON(item):
+'''def toJSON(item):
     if isinstance(item, dict):
-        result = "{ "
-        lastKey = list(item.keys())[-1]
-        for key in item:
-            if key == lastKey:
-                result += "\"{0}\": \"{1}\"".format(key, item[key])
-            else:
-                result += "\"{0}\": \"{1}\", ".format(key, item[key])
-        result += " }"
-        return result
+        return json.dumps(item)
 
     elif isinstance(item, list):
         result = "[ "
@@ -40,7 +33,7 @@ def toJSON(item):
         return result
 
     else:
-        return "\"" + str(item) + "\""
+        return "\"" + str(item) + "\""'''
 
 
 def getAllProbs():
@@ -57,7 +50,7 @@ def getAllProbs():
 
 # 백준에 존재하는 모든 문제 목록을 JSON 형식의 문자열로 반환합니다.
 def getAllProb_JSON():
-    return toJSON(getAllProbs())
+    return json.dumps(getAllProbs())
 
 
 # 우리학교에서 푼 문제의 ID(문자열)를 담은 리스트를 반환합니다.
@@ -70,7 +63,7 @@ def getUOSProbList():
 
 # 위의 getUOSProblemsList()과 목적은 같지만 반환 형식이 JSON 형식의 문자열입니다.
 def getUOSProbList_JSON():
-    return toJSON(getUOSProbList())
+    return json.dumps(getUOSProbList())
 
 
 # 백준 채점 시스템에서 "맞았습니다!!"를 받은 우리학교의 마지막 제출번호를 반환합니다.
@@ -85,23 +78,29 @@ def getLastProbID():
 
 # 지난 마지막 제출번호(subID) 이후에 우리학교에서 제출하여 "맞았습니다!!"를 받은 문제의 리스트를 반환합니다. (중복 없음)
 def getSolvedAfter(subID):
-    return bapi.getSolvedAfter(subID)
+    return list(bapi.getSolvedAfter(subID))
 
 
 # 위의 getSolvedAfter((string) subID)와 목적은 같지만 반환 형식이 JSON 형식의 문자열입니다.
 def getSolvedAfter_JSON(subID):
-    pass
+    return json.dumps(getSolvedAfter(subID))
 
 
+# 지난 마지막 문제 번호(probID) 이후에 새로 추가된 문제 목록을 dictionary를 원소로 하는 리스트 형식으로 반환합니다.
+# (백준의 "추가된 문제", "추가된 영어 문제"에서 조회하므로, 너무 오래전의 문제 번호를 입력하면 문제를 모두 조회하지 못할 수 있습니다.)
 def getProbsAfter(probID):
-    pass
+    baekList = bapi.getProbsAfter(probID)
+    solvedList = sapi.getAllProbs()
+    baekList.sort(key=lambda x: int(x["id"]))
+
+    for prob in solvedList:
+        index = searchID(prob["id"], baekList)
+        if index is not None:
+            baekList[index]["rank"] = prob["rank"]
+
+    return baekList
 
 
-# 지난 마지막 문제 번호(probID) 이후에 새로 추가된 문제 목록을 JSON 형식으로 반환합니다.
+# 위의 getProbsAfter((string) probID)와 목적은 같지만 반환 형식이 JSON 형식의 문자열입니다.
 def getProbsAfter_JSON(probID):
-    pass
-
-
-# id에 해당하는 문제의 정보를 새로 가져옵니다.
-def getProbInfo(id):
-    pass
+    return json.dumps(getProbsAfter(probID))
