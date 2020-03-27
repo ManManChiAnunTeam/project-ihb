4d9bc65e1afa07240fa3baf3260399639f197812
powergee <49356944+powergee@users.noreply.github.com>
2020-03-28 00:03:07 +0900

    Implement all functions in README

--- CrawlingAPI/BaekjoonAPI.py
+++ CrawlingAPI/BaekjoonAPI.py
@@ -11,9 +11,9 @@ NEWPROB_URL = "https://www.acmicpc.net/newproblems"
 NEWPROBENG_URL = "https://www.acmicpc.net/newproblems/english"
 
 
-def getProbsInPage(page):
+def getProbsInPage(url):
     probs = []
-    html = urlopen(PROBLEMSET_URL + str(page))
+    html = urlopen(url)
     bs = BeautifulSoup(html, "lxml")
     pSet = bs.find(id="problemset").find_all("tr")
 
@@ -31,7 +31,7 @@ def getAllProbs():
     probs = []
     page = 1
     while True:
-        found = getProbsInPage(page)
+        found = getProbsInPage(PROBLEMSET_URL + str(page))
         if len(found) == 0:
             break
         probs += found
@@ -102,16 +102,48 @@ def getLastProbID():
         return id2
 
 
-def getSolved(top):
-    # not implemented
+# top(채점번호)부터 채점번호가 감소하는 방향으로 채점 현황의 한 페이지를 크롤링함.
+# 반환값은 (Boolean: 페이지를 모두 크롤링 하였는지 여부, String: 크롤링 한 제출의 가장 마지막(번호가 가장 작은) 번호) 형식의 튜플이다.
+# 반환 튜블의 Boolean에 대하여: 채점번호가 endOfSub 이하인 제출을 발견하면 크롤링을 중단하고 False를 반환한다. (그 외, True 반환)
+def getSolved(top, resultSet, endOfSub=""):
     html = None
     if top is None:
         html = urlopen(STATUS_URL)
     else:
         html = urlopen(STATUS_URL + "&top=" + top)
     bs = BeautifulSoup(html, "lxml")
-    pass
+    subs = bs.find(id="status-table").find("tbody").find_all("tr")
+
+    canReadMore = True
+    minSub = None
+    for sub in subs:
+        info = sub.find_all("td")
+        subID = info[0].getText()
+        if subID == endOfSub:
+            canReadMore = False
+            break
+        resultSet.add(info[2].find("a").getText())
+        minSub = subID
+
+    return (canReadMore, minSub)
 
 
 def getSolvedAfter(subID):
-    pass
+    result = set()
+    top = getLastSubID()
+
+    while True:
+        readAll, minSub = getSolved(top, result, subID)
+        if not readAll:
+            break
+        top = minSub
+
+    return result
+
+
+def getProbsAfter(probID):
+    result = []
+    probID_int = int(probID)
+    result += [prob for prob in getProbsInPage(NEWPROB_URL) if int(prob["id"]) > probID_int]
+    result += [prob for prob in getProbsInPage(NEWPROBENG_URL) if int(prob["id"]) > probID_int]
+    return result
