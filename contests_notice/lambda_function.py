6249d92d2b59033229add150a1136075413163cb
Moonki Choi <52124204+iknoom@users.noreply.github.com>
2020-06-03 23:04:18 +0900

    Update lambda_function.py

--- contests_notice/lambda_function.py
+++ contests_notice/lambda_function.py
@@ -5,8 +5,7 @@ from datetime import datetime
 
 codeforces_url = "https://codeforces.com"
 atcoder_url = "https://atcoder.jp"
-slack_url = "https://hooks.slack.com/services/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"
-#slack_url = "https://hooks.slack.com/services/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"
+slack_url = ""
 channel_ID = {'Codeforces' : 'C0105GWQM28', 'Atcoder' : 'C01062ANCCD'}
 s3 = boto3.client('s3')
 
